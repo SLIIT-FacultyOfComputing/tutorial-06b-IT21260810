3f579692f49bb1c3e7e2e1299e9a3ace4d924ad4
IT21260810 <99312190+IT21260810@users.noreply.github.com>
2022-03-19 14:21:08 +0000

    Completed Student.cpp

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,17 @@
-#include "Box.h"
+class Box {
+    private:
+       int length;
+       int width;
+       int height;
+    public:
+       // write prototypes of setters for length, width and height
+      void setLength(int l) ;
+      void setWidth(int w) ;
+      void setHeight(int h) ;
+       // write prototypes of getters for length, width and height
+      int getLength() ;
+      int getWidth() ;
+      int getHeight() ;
 
-// Implement setters and getters
-
-// Implemenet the calcVolume() unction
-int Box::calcVolume() {
-}
+      int calcVolume();
+};
